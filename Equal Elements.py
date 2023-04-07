f5a77ee18358ddc74dabcdd6f54bea538866f5ac
Madhu HS <86066580+Madhu-2101@users.noreply.github.com>
2023-04-07 23:35:48 +0530

    Update and rename Equal Elements to Equal Elements.py

--- Equal Elements.py
+++ Equal Elements.py
@@ -1,6 +1,7 @@
-You are given an array A of size N. In one operation, you can do the following:
-Select indices i and j (i≠j) and set Ai = Aj.
-Find the minimum number of operations required to make all elements of the array equal.
+# You are given an array A of size N. In one operation, you can do the following:
+# Select indices i and j (i≠j) and set Ai = Aj.
+# Find the minimum number of operations required to make all elements of the array equal.
+
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 from collections import Counter
