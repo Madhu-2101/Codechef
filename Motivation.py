221851a2ec73190fa818e12b65d80f161261a6ac
Madhu HS <86066580+Madhu-2101@users.noreply.github.com>
2023-04-13 20:58:18 +0530

    Update and rename Motivation to Motivation.py

--- Motivation.py
+++ Motivation.py
@@ -1,5 +1,5 @@
-Chef has been searching for a good motivational movie that he can watch during his exam time. His hard disk has X GB of space remaining. His friend has N movies 
-represented with (Si,Ri) representing (space required, IMDB rating). Help Chef choose the single best movie (highest IMDB rating) that can fit in his hard disk.
+# Chef has been searching for a good motivational movie that he can watch during his exam time. His hard disk has X GB of space remaining. His friend has N movies 
+# represented with (Si,Ri) representing (space required, IMDB rating). Help Chef choose the single best movie (highest IMDB rating) that can fit in his hard disk.
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
